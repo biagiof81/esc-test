4c1e4c65651b276b17cac1e23eb5bb99af785aaa
biagiof81 <39884940+biagiof81@users.noreply.github.com>
2018-06-05 10:35:44 +0200

    change business logic to toggle activity user

--- esc-test-ejb/src/main/java/it/biagio/esc/test/ejb/dao/UserDao.java
+++ esc-test-ejb/src/main/java/it/biagio/esc/test/ejb/dao/UserDao.java
@@ -22,63 +22,65 @@ import it.biagio.esc.test.ejb.entity.User;
 @Stateless
 public class UserDao {
 
-	/*
-	 * @Inject
-	 * 
-	 * @PersistenceContext(unitName="escPU")
-	 */
-	protected EntityManager entityManager;
+    /*
+     * @Inject
+     * 
+     * @PersistenceContext(unitName="escPU")
+     */
+    protected EntityManager entityManager;
 
-	protected EntityManagerFactory factory;
+    protected EntityManagerFactory factory;
 
-	protected CriteriaBuilder builder;
+    protected CriteriaBuilder builder;
 
-	@PostConstruct
-	protected void initialize() {
-		factory = Persistence.createEntityManagerFactory("escPU");
-		entityManager = factory.createEntityManager();
-		builder = entityManager.getCriteriaBuilder();
-	}
+    @PostConstruct
+    protected void initialize() {
+        factory = Persistence.createEntityManagerFactory("escPU");
+        entityManager = factory.createEntityManager();
+        builder = entityManager.getCriteriaBuilder();
+    }
 
-	public List<User> getUsersByCoordinate(Location location1, Location location2,Long dateFrom,Long dateTo) {
+    public List<User> getUsersByCoordinate(Location location1, Location location2, Long dateFrom, Long dateTo) {
 
-		CriteriaQuery<User> query = builder.createQuery(User.class);
+        CriteriaQuery<User> query = builder.createQuery(User.class);
 
-		Root<User> root = query.from(User.class);
+        Root<User> root = query.from(User.class);
 
-		Join<User, Location> location = root.join("location");
-		query.where(builder.and(whereCondition(location, location1, location2,dateFrom,dateTo)));
-		query.distinct(true);
-		return entityManager.createQuery(query).getResultList();
+        Join<User, Location> location = root.join("location");
+        query.where(builder.and(whereCondition(location, location1, location2, dateFrom, dateTo)));
+        query.distinct(true);
+        return entityManager.createQuery(query).getResultList();
 
-	}
+    }
 
-	/**
-	 * forse metterei pure il lock
-	 * 
-	 * @return
-	 */
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public User toggle(User user) {
+    /**
+     * forse metterei pure il lock
+     * 
+     * @return
+     */
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public User toggle(User user) {
 
-		entityManager.merge(user);
+        User savedUser = entityManager.find(User.class, user.getId());
+        savedUser.setActive(user.getActive());
 
-		return entityManager.merge(user);
+        return entityManager.merge(savedUser);
 
-	}
+    }
 
-	public Predicate whereCondition(Join<?, ?> root, Location location1, Location location2, Long dateFrom, Long dateTo) {
-		Path<Double> longitudine = root.get("longitudine");
-		Path<Double> latitude = root.get("latitude");
-		Path<Long> insdate = root.get("insdate");
-		return builder.and(
-				builder.and(builder.greaterThanOrEqualTo(longitudine, location1.getLongitudine()),
-						builder.lessThanOrEqualTo(longitudine, location2.getLongitudine())),
-				builder.and(builder.lessThanOrEqualTo(latitude, location1.getLatitude()),
-						builder.greaterThanOrEqualTo(latitude, location2.getLatitude())),
-				builder.and(builder.greaterThanOrEqualTo(insdate,dateFrom ),
+    public Predicate whereCondition(Join<?, ?> root, Location location1, Location location2, Long dateFrom,
+            Long dateTo) {
+        Path<Double> longitudine = root.get("longitudine");
+        Path<Double> latitude = root.get("latitude");
+        Path<Long> insdate = root.get("insdate");
+        return builder.and(
+                builder.and(builder.greaterThanOrEqualTo(longitudine, location1.getLongitudine()),
+                        builder.lessThanOrEqualTo(longitudine, location2.getLongitudine())),
+                builder.and(builder.lessThanOrEqualTo(latitude, location1.getLatitude()),
+                        builder.greaterThanOrEqualTo(latitude, location2.getLatitude())),
+                builder.and(builder.greaterThanOrEqualTo(insdate, dateFrom),
                         builder.lessThanOrEqualTo(insdate, dateTo)));
 
-	}
-	
+    }
+
 }
